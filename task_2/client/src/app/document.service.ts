b8efcc59f5cd152d83f21c622d37348591cad388
PranjalSrivastava092 <pranjalsri092@gmail.com>
2019-08-10 16:19:20 +0530

    Completed Task 2

--- task_2/client/src/app/document.service.ts
+++ task_2/client/src/app/document.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
-import { Document } from '../document';
+import { Document } from './document';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
+	currentDocument = this.socket.fromEvent<Document>('document');
+	documents = this.socket.fromEvent<string[]>('documents');
 
-  constructor() { }
+  constructor(private socket: Socket) { }
+
+  getDocument(id: string){
+  	this.socket.emit('get', id);
+  }
+
+  newDocument(){
+  	this.socket.emit('add', {id: this.docId(), doc: ''});
+  }
+
+  editDocument(document: Document){
+  	this.socket.emit('edit', document);
+  }
+
+   private docId() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    
+    return text;
+  }
+
 }
